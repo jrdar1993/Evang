2b91ba438f594d0d8d8a14ec5502416482aa8643
jrdar1993 <68728916+jrdar1993@users.noreply.github.com>
2025-06-17 10:29:46 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import os
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:cantares2025.@db.zwmizphrwirvggmbkumm.supabase.co:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres.zwmizphrwirvggmbkumm:cantares2025.@aws-0-us-east-2.pooler.supabase.com:6543/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "supersecreto"
